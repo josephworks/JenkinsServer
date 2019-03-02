2e7f5826b8619ca69a38f3b084aded700ebbc899
Joseph Robinson <smartech56@gmail.com>
2019-03-01 22:13:42 -0700

    updated start script

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Starting!"
-echo "Updating jenkins server!"
+echo "Updating / Installing jenkins server!"
 rm jenkins.war
 wget http://mirrors.jenkins.io/war/latest/jenkins.war
-echo "Open your browser and connect to http://localhost:8080 to use your jenkins server!"
 java -jar jenkins.war
+echo "Open your browser and connect to http://localhost:8080 to use your jenkins server!"
